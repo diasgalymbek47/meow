d6b23148d30051658cb92db1f47e182d860fffb3
ADok <adoktaz11@gmail.com>
2024-12-05 18:59:06 +0500

    Добавил немного Стилей в Main

--- src/components/HeaderHome.vue
+++ src/components/HeaderHome.vue
@@ -4,16 +4,14 @@ import { RouterLink } from 'vue-router';
 
 <template>
     <header class="header">
-        <RouterLink to="/">
-            <h1 class="logo">Meow</h1>
-        </RouterLink>
+        <RouterLink to="/"><h1 class="logo">Meow</h1></RouterLink>
 
         <nav class="nav">
             <RouterLink class="nav_item" to="/music">Музыка</RouterLink>
             <RouterLink class="nav_item" to="/chat">Чат</RouterLink>
             <RouterLink class="nav_item" to="/cinema">Кино</RouterLink>
-            <RouterLink class="nav_item login" to="/login">Войти</RouterLink>
         </nav>
+        <RouterLink class="login" to="/login">Войти</RouterLink>
     </header>
 </template>
 
@@ -22,21 +20,55 @@ import { RouterLink } from 'vue-router';
     padding: 1em;
     display: flex;
     align-items: center;
-    gap: 5em;
+    justify-content: space-between;
 }
 
 .logo {
     margin: 0;
+    color: #201B65;
 }
 
 .nav {
-    width: 100%;
     display: flex;
     align-items: center;
-    gap: 4em;
+}
+.nav_item {
+    color: #201B65;
+    width: 100px;
+    padding: .75em 0;
+    overflow: hidden;
+    transition: all 0.3s ease;
+}
+.nav_item::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 1px;
+    opacity: 0;
+    position: relative;
+    bottom: -8px;
+    background-color: #5F5BFF;
+    transform: translateX(-100%);
+    transition: all 0.3s ease;
+}
+.nav_item:hover {
+    color: #5F5BFF;
+}
+.nav_item:hover::after {
+    transform: translateX(0%);
+    opacity: 1;
 }
 
 .login {
-    margin-left: auto;
+    padding: 13px 23px;
+    background-color: #5F5BFF;
+    border-radius: 8px;
+    color: #F6F6F6;
+    box-shadow: 4px 4px 10px rgba(0, 0, 0, 25%);
+    transition: transform 0.1s ease;
+}
+.login:active {
+    transform: translateY(3px) translateX(3px);
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 25%);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,78 @@ import Header from '../components/HeaderHome.vue'
 </script>
 
 <template>
-  <Header/>
-  <main>
-    <h2>Я главный на этом районе</h2>
-  </main>
+    <div class="wrapper">
+        <div class="container">
+            <Header />
+            <main>
+                <h6>Добро пожаловать в <span>Meow</span>!</h6>
+                <h2>Слушай <span>музыку</span>, смотри
+                    <br> <span>фильмы</span> и общайся с <span>друзьями</span>.
+                    <br> Всё в одном месте!
+                </h2>
+                <p>Присоединяйся к Meow сегодня и будь в центре событий!
+                    <br> Зарегистрируйся сейчас и начни своё путешествие!
+                </p>
+                <RouterLink class="login" to="/login"><button>Начать</button></RouterLink>
+            </main>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.wrapper {
+    min-height: 100vh;
+    position: relative;
+    background: left / 18% no-repeat url("/src/components/icons/main-rightside-img.png"),
+        var(--main-bg-page) right / 18% no-repeat url("/src/components/icons/main-leftside-img.png");
+}
+main {
+    margin-top: 200px;
+    color: #201B65;
+}
+span {
+    color: #5F5BFF;
+}
+
+h6 {
+    width: 267px;
+    padding: 10px 0px;
+    margin: 0 auto;
+    font-size: 14px;
+    font-weight: normal;
+    border-radius: 40px;
+    border: 1px solid #E4E4E4;
+    background-color: #fff;
+}
+
+h2 {
+    margin-top: 40px;
+    line-height: 1.2;
+    font-size: 48px;
+}
+
+p {
+    margin-top: 50px;
+    font-size: 20px;
+    letter-spacing: 0.4px;
+    line-height: 1.5;
+}
+
+button {
+    margin-top: 60px;
+    width: 260px;
+    height: 70px;
+    font-size: 24px;
+    border-radius: 10px;
+    color: #fff;
+    outline: none;
+    border: none;
+    background-color: #000267;
+    box-shadow: 4px 4px 5px rgba(0, 0, 0, 25%);
+    transition: transform 0.1s ease;
+}
+button:active {
+    transform: translateY(3px) translateX(3px);
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 25%);
+}
+</style>
