99bae2f09861f91b8416d608ae29677294a4790f
Galymbek Dias <diasgalymbek47@gmail.com>
2024-12-19 16:29:42 +0500

    new build

---
+++ assets/CinemaView-sIIhTtGA.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c,o}from"./index-B8wQtOYQ.js";const r={};function n(t,a){return o(),c("h2",null,"Я страница для кино")}const f=e(r,[["render",n]]);export{f as default};
